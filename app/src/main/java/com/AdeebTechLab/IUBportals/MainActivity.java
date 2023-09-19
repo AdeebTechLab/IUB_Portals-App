52beff88613858a880cddefbf81badcf9b631bc8
Adeeb <AdeebtechnologyLab@gmail.com>
2023-09-19 14:57:35 +0500

    5th

--- app/src/main/java/com/AdeebTechLab/IUBportals/MainActivity.java
+++ app/src/main/java/com/AdeebTechLab/IUBportals/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
+        getSupportActionBar().setTitle("All Portal The Islamia University of Bahawalpur");
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
